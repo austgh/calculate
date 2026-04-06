057b788fce01130db3a18aae2e5e292f9cae37a7
austgh <1343685859@qq.com>
2026-04-06 17:36:21 +0800

    还款功能参数

--- src/main/java/com/ahzx/entity/Repayment.java
+++ src/main/java/com/ahzx/entity/Repayment.java
@@ -30,12 +30,14 @@ public class Repayment {
      */
     public int specifiedCycle;
     /*
-     *1. 天 2. 月 3. 年 与指定周期搭配使用。
+     *指定期限单位
+     * 1. 天 2. 月 3. 年 与指定周期搭配使用。
      */
     public int specifiedCycleUnit;
     /*
     *
-     * 区段期限标识 1.贷款期限 2.区段期限 3.指定期限
+     * 区段期限标识
+     * 1.贷款期限 2.区段期限 3.指定期限
      */
     public String segmentDesignatedTermUnit;
 
@@ -43,11 +45,15 @@ public class Repayment {
     public String segmentDesignatedAmountIndicator;
     //当指定区段归还本金时，该字段为必输。
     public int segmentSpecificDesignatedAmount;
-    //01.最早放款当月开始还款 02.最早放款下月开始还款
+    //首期标识
+    // 01.最早放款当月开始还款 02.最早放款下月开始还款
     public String firstPeriodIndicator;
-    //1.最后一期合并 2.最后一期拆分 3.自动顺延直至贷款到期
+    //末期标识
+    // 1.最后一期合并 2.最后一期拆分 3.自动顺延直至贷款到期
     public String finalPeriodIndicator;
-    //宽限期内不计罚息和复息，宽限期内还款等同于还款日还款。超过宽限期仍未还款，则按照罚息利率补计宽限期内的罚息复息，且贷款逾期天数从结息日开始计算。
+
+    //宽限期天数
+    // 宽限期内不计罚息和复息，宽限期内还款等同于还款日还款。超过宽限期仍未还款，则按照罚息利率补计宽限期内的罚息复息，且贷款逾期天数从结息日开始计算。
     public int gracePeriodDays;
 
 
